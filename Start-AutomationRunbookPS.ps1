6d793d2923e0f6290a82cca07939e0d21038e2e8
DESKTOP-13M3SH2\Admin <verboonalex@gmail.com>
2017-03-05 15:38:00 +0100

    Initial test scripts

---
+++ Start-AutomationRunbookPS.ps1
@@ -0,0 +1,29 @@
+﻿<#
+Sample script to start an Azure Automation Job from PowerShell
+
+#>
+
+$runbookName = "Start-AzureCloudVM"
+$RbResourceGroupName = "RG_automation01"
+$AutomationAcct = "Automation01"
+$VMName = "win10vm2"
+$ResourceGroupName = "RG_win10vm02"
+
+
+$params = @{
+    Name = $VMName;
+    ResourceGroupName = $ResourceGroupName;
+}
+
+
+$job = Start-AzureRmAutomationRunbook –AutomationAccountName $AutomationAcct -Name $runbookName -ResourceGroupName $RbResourceGroupName -Parameters $params
+
+$doLoop = $true
+While ($doLoop) {
+   $job = Get-AzureRmAutomationJob –AutomationAccountName $AutomationAcct -Id $job.JobId -ResourceGroupName $RbResourceGroupName
+   $status = $job.Status
+   $doLoop = (($status -ne "Completed") -and ($status -ne "Failed") -and ($status -ne "Suspended") -and ($status -ne "Stopped"))
+}
+
+Get-AzureRmAutomationJobOutput –AutomationAccountName $AutomationAcct -Id $job.JobId -ResourceGroupName $RbResourceGroupName –Stream Output 
+
